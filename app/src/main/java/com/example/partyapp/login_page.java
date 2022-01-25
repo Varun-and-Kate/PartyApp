f1112d0e4407f169c9f59b4db28d2110f2d3865e
Kate_Kim <gxk180021@utdallas.edu>
2022-01-24 21:43:57 -0600

    changed text type in forgot password page and login page, made design for party list page and add some functions to go to other activity pages

--- app/src/main/java/com/example/partyapp/login_page.java
+++ app/src/main/java/com/example/partyapp/login_page.java
@@ -2,13 +2,50 @@ package com.example.partyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class login_page extends AppCompatActivity {
+    EditText username_input;
+    TextView textName;
+    private Button loginButton;
+    private Button forgotPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        forgotPasswordButton = (Button) findViewById(R.id.forgotPasswordButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPartyList();
+            }
+        });
+
+        forgotPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openForgotPassword();
+            }
+        });
+
+    }
+
+    public void openPartyList() {
+        Intent intent = new Intent(this, party_list_page.class);
+        startActivity(intent);
+    }
+
+    public void openForgotPassword() {
+        Intent intent = new Intent(this, forgot_password.class);
+        startActivity(intent);
     }
 }

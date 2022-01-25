651801c0db26da339ebcf51b92138d6a5af3fdbd
VarunTodupunoori <vtodupprivate@gmail.com>
2022-01-24 19:52:49 -0600

    Design for forgot password

---
+++ app/src/main/java/com/example/partyapp/forgot_password.java
@@ -0,0 +1,14 @@
+package com.example.partyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgot_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password);
+    }
+}

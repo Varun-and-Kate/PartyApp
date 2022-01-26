10241fc5477a0f2f38c8292abc7abbf168304bfd
Kate_Kim <gxk180021@utdallas.edu>
2022-01-25 21:34:12 -0600

    created filter page

---
+++ app/src/main/java/com/example/partyapp/filter_page.java
@@ -0,0 +1,19 @@
+package com.example.partyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class filter_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filter_page);
+
+
+    }
+
+
+}

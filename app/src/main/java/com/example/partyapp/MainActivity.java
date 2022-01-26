b00e0b9319335faf0e40917840d7b532cdd1c03a
VarunTodupunoori <vtodupprivate@gmail.com>
2022-01-25 21:24:12 -0600

    used function to switch to login page when user clicks anywhere on the main loading page

--- app/src/main/java/com/example/partyapp/MainActivity.java
+++ app/src/main/java/com/example/partyapp/MainActivity.java
@@ -8,22 +8,29 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Handler h = new Handler();
-    @Override
-    protected void onCreate(Bundle savedInstantState){
+
+    protected void onCreate(Bundle savedInstantState) {
         super.onCreate(savedInstantState);
         setContentView(R.layout.main_loading);
-        h.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, reset_password.class);
-                startActivity(i);
-                finish();
 
+        RelativeLayout rlayout = (RelativeLayout) findViewById(R.id.mainlayout);
+        rlayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                open_login_page1();
             }
-        }, 3000);
+
+        });
+    }
+
+    public void open_login_page1() {
+        Intent intent = new Intent(this, login_page.class);
+        startActivity(intent);
+
     }
 }

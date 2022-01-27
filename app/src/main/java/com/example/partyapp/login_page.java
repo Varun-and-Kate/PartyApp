82aac7ee6d84a2b3b755a7a1a6511ea203a5f74d
VarunTodupunoori <vtodupprivate@gmail.com>
2022-01-26 22:41:36 -0600

    added database to check for login: username and password. Also check for signing up. Made a DBhelper class

--- app/src/main/java/com/example/partyapp/login_page.java
+++ app/src/main/java/com/example/partyapp/login_page.java
@@ -8,24 +8,53 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class login_page extends AppCompatActivity {
-    EditText username_input;
-    TextView textName;
-    private Button loginButton;
-    private Button forgotPasswordButton;
+    EditText username, password;
+    Button loginButton;
+    Button signupbutton;
+    Button forgotPasswordButton;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        username = (EditText) findViewById(R.id.username_input);
+        password = (EditText) findViewById(R.id.password1);
         loginButton = (Button) findViewById(R.id.loginButton);
+        signupbutton = (Button) findViewById(R.id.signupbut);
         forgotPasswordButton = (Button) findViewById(R.id.resetbutton);
+        DB = new DBHelper(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openPartyList();
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(login_page.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(login_page.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
+                        openPartyList();
+                    } else {
+                        Toast.makeText(login_page.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignup();
             }
         });
 
@@ -45,6 +74,11 @@ public class login_page extends AppCompatActivity {
 
     public void openForgotPassword() {
         Intent intent = new Intent(this, forgot_password.class);
+        startActivity(intent);
+    }
+
+    public void openSignup(){
+        Intent intent = new Intent(this, signup.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/partyapp/signup.java
+++ app/src/main/java/com/example/partyapp/signup.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class signup extends AppCompatActivity {
-    private Button button;
-    private ImageButton back;
+
+    Button button;
+    ImageButton back;
+    EditText username, password, repassword;
+    DBHelper DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        repassword = (EditText) findViewById(R.id.confpassword);
+        DB = new DBHelper(this);
 
         back = (ImageButton) findViewById(R.id.backarrow);
         back.setOnClickListener(new View.OnClickListener() {
@@ -29,7 +40,32 @@ public class signup extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                open_login_page2();
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(signup.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(checkuser == false){
+                            Boolean insert = DB.insertData(user, pass);
+                            if(insert==true){
+                                Toast.makeText(signup.this, "Registered Sucessfully", Toast.LENGTH_SHORT).show();
+                                open_login_page2();
+
+                            } else{
+                                Toast.makeText(signup.this, "Resistration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(signup.this, "User already exists please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    } else{
+                        Toast.makeText(signup.this, "Password not matching", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
             }
         });
     }
